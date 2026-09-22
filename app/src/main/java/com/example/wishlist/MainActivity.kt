ebcff9653cf3f7aca2fcc00877cb28f68a27d9ac
saanyay <saanyajauhri@gmail.com>
2026-09-22 19:35:56 -0400

    Add item to wishlist

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
 
@@ -23,8 +25,40 @@ class MainActivity : AppCompatActivity() {
         val wishlistAdapter =
             WishlistAdapter(wishlistItems)
 
+        val itemNameEditText =
+            findViewById<EditText>(R.id.itemNameEditText)
+
+        val itemPriceEditText =
+            findViewById<EditText>(R.id.itemPriceEditText)
+
+        val itemUrlEditText =
+            findViewById<EditText>(R.id.itemUrlEditText)
+
+        val submitButton =
+            findViewById<Button>(R.id.submitButton)
+
         wishlistRecyclerView.adapter = wishlistAdapter
         wishlistRecyclerView.layoutManager = LinearLayoutManager(this)
+
+        submitButton.setOnClickListener {
+
+            val name = itemNameEditText.text.toString()
+            val price = itemPriceEditText.text.toString()
+            val url = itemUrlEditText.text.toString()
+
+            if (name.isNotBlank() && price.isNotBlank() && url.isNotBlank()) {
+
+                val newItem = WishlistItem(name, price, url)
+
+                wishlistItems.add(newItem)
+
+                wishlistAdapter.notifyItemInserted(wishlistItems.size - 1)
+
+                itemNameEditText.text.clear()
+                itemPriceEditText.text.clear()
+                itemUrlEditText.text.clear()
+            }
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
